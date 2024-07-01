f8039e765a3842d82dcc06c2061d28443d9f4766
Subhankar Sarkar <162408654+SubhankarSarkar01@users.noreply.github.com>
2024-07-01 16:12:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+
+console.log("subhankar")
+
+let name = "subhankar"
+let age = 24
+let isloggedIn = false
+
+// number = 2 to power 53
+// bigint
+//  string  = ""
+// boolean = true/ false
+// null = standalone value
+// undefined
+// symbol = unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
