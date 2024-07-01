37735ac5c45d72a3f282a06571813ac84e69ffa6
Subhankar Sarkar <162408654+SubhankarSarkar01@users.noreply.github.com>
2024-07-01 20:04:50 +0000

    conversion done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "22"
+console.log(typeof score)
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// "22abc" =>NaN
+// true =>1; false =>0
+// "" => false
+// "subhankar" = true
+
+// ------ operations -------
+let str1 = "hello" 
+let str2 = " subhankar"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2+2)
+console.log(1 + 2 +"2")
+
