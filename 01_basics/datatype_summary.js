7aa394061b0f02784bb98a342320c639f391b0a5
Subhankar Sarkar <162408654+SubhankarSarkar01@users.noreply.github.com>
2024-07-03 13:51:24 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,26 @@
+// Primitive
+// 7 types : String , Number , boolean, null, undefined, symbol, bigInt
+const str = "subhankar"
+const num = 100
+const isloggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigInt = 234234234324n
+
+
+// Reference (Non - Primitive)
+// 3 types : Array, object, function
+
+const arr = ["apple","Lichi"];
+let myObj = {
+    name : "subhankar",
+    Id : "23"
+}
+
+let myfunction = function () {
+    console.log("Hello World")
+}
