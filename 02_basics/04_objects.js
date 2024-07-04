afc8afd3351de0dd7ac93077023c2c7fc9757f7e
Subhankar Sarkar <162408654+SubhankarSarkar01@users.noreply.github.com>
2024-07-04 19:38:43 +0000

    object middle done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,44 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Subhankar"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "subhankar",
+            lastname : "sarkar"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "c",4: "d"}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3)
+
+const users = [
+    {
+        id : 1,
+        email: "some@gmail.com"
+    },
+    {
+        id : 2,
+        email : "sometwo@gmail.com"
+    }
+]
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
