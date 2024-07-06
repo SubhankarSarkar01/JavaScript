f29b4bca4fe02d96a2022dcd296d528014b6314a
Subhankar Sarkar <162408654+SubhankarSarkar01@users.noreply.github.com>
2024-07-06 17:19:43 +0000

    do-while done

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let i = 0
+// while(i<=10){
+//     console.log(`Value of i is ${i}`)
+//     i = i + 2
+// }
+
+let myArray = ['flash','Batman','superman']
+
+let arr = 0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+let score = 12
+do{
+    console.log(`Score is ${score}`)
+    score = score + 1
+}while(score<=10)
